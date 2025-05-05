b0901cbd3def52bbf3798f044a89aa30de214e0e
aryans-15 <aryan.sharma0714@gmail.com>
2025-05-05 10:40:53 -0400

    p1-p15 editorials

--- p01-p10/p07/p07.py
+++ p01-p10/p07/p07.py
@@ -1,8 +1,7 @@
 import math
 
 N, c = 10001, 0
-# Dusart's 1999 extension of Rosser's theorem
-ub = math.ceil(N * (math.log(N) + math.log(math.log(N)) - 1))
+ub = math.ceil(N * math.log(N) + N * math.log(math.log(N)) - 1)
 A, ub2 = [True] * ub, int(ub ** 0.5) + 1
 
 for p in range(2, ub2):
